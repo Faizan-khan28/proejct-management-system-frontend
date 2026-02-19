9f544a24937212269fd0e7d1ef6d7bd883600a47
faizanKhan <faizankhan46849@gmail.com>
2026-02-19 22:39:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center h-screen flex justify-center items-center">Product Management System</h1>
+    </>
+  )
+}
+
+export default App
